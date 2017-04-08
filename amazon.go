64102c8263a84a181fc60e511e282a380dcdbddd
Yoshitake Hamano <oed0cow6oy5@gmail.com>
2017-04-08 22:24:44 +0900

    Add logs

--- amazon.go
+++ amazon.go
@@ -13,19 +13,25 @@ func main() {
 	defer driver.Stop()
 
 	page, err := driver.NewPage()
+	log.Print("page, err := driver.NewPage()")
 	if err != nil {
 		log.Fatalf("Failed to open page: %v", err)
 	}
+	log.Print("page.Navigate(https://www.amazon.co.jp)")
 	if err := page.Navigate("https://www.amazon.co.jp"); err != nil {
 		log.Fatalf("Failed to navigate: %v", err)
 	}
 
+	log.Print("page.FindByID(twotabsearchtextbox).Fill(golang)")
 	if err := page.FindByID("twotabsearchtextbox").Fill("golang"); err != nil {
 		log.Fatalf("Failed to search: %v", err)
 	}
+	log.Print("page.FindByID(twotabsearchtextbox).Submit()")
 	if err := page.FindByID("twotabsearchtextbox").Submit(); err != nil {
 		log.Fatalf("Failed to submit: %v", err)
 	}
-	page.Screenshot("golang.png")
+	if err := page.Screenshot("golang.png"); err != nil {
+		log.Fatalf("Failed to screenshot: %v", err)
+	}
 	log.Print("Successed.")
 }
