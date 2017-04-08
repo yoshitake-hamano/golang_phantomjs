a51062f1d4685018fab1a01c61393d3d2b662aeb
Yoshitake Hamano <oed0cow6oy5@gmail.com>
2017-04-09 01:58:51 +0900

    Implement main.go

--- amazon.go
+++ amazon.go
@@ -6,6 +6,7 @@ import (
 )
 
 func main() {
+	// START OMIT
 	driver := agouti.PhantomJS()
 	if err := driver.Start(); err != nil {
 		log.Fatalf("Failed to start driver: %v", err)
@@ -13,25 +14,27 @@ func main() {
 	defer driver.Stop()
 
 	page, err := driver.NewPage()
-	log.Print("page, err := driver.NewPage()")
+	log.Print("page, err := driver.NewPage()") // OMIT
 	if err != nil {
 		log.Fatalf("Failed to open page: %v", err)
 	}
-	log.Print("page.Navigate(https://www.amazon.co.jp)")
+	log.Print("page.Navigate(https://www.amazon.co.jp)") // OMIT
 	if err := page.Navigate("https://www.amazon.co.jp"); err != nil {
 		log.Fatalf("Failed to navigate: %v", err)
 	}
 
-	log.Print("page.FindByID(twotabsearchtextbox).Fill(golang)")
+	log.Print("page.FindByID(twotabsearchtextbox).Fill(golang)") // OMIT
 	if err := page.FindByID("twotabsearchtextbox").Fill("golang"); err != nil {
 		log.Fatalf("Failed to search: %v", err)
 	}
-	log.Print("page.FindByID(twotabsearchtextbox).Submit()")
+	log.Print("page.FindByID(twotabsearchtextbox).Submit()") // OMIT
 	if err := page.FindByID("twotabsearchtextbox").Submit(); err != nil {
 		log.Fatalf("Failed to submit: %v", err)
 	}
+	log.Print("page.Screenshot(golang.png)") // OMIT
 	if err := page.Screenshot("golang.png"); err != nil {
 		log.Fatalf("Failed to screenshot: %v", err)
 	}
-	log.Print("Successed.")
+	log.Print("Successed.") // OMIT
+	// END OMIT
 }
